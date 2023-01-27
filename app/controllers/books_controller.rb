458cf9095b442ad65500e0bee36bf8063ebb0a37
EC2 Default User <ec2-user@ip-172-31-0-10.ap-northeast-1.compute.internal>
2023-01-27 15:50:43 +0000

    [Fix-finish]task3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,11 +10,10 @@ class BooksController < ApplicationController
   end
 
   def edit
-    user_id = params[:id].to_i
-    unless user_id == current_user.id
+    @book = Book.find(params[:id])
+    unless @book.user_id == current_user.id
       redirect_to books_path
     end
-    @book = Book.find(params[:id])
   end
   
   def create
